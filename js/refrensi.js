219f4d27c9b087056f360e5caf4155e7d4a1a1df
simitsu-ui <86468000+simitsu-ui@users.noreply.github.com>
2021-06-25 16:40:59 +0700

    update

--- js/refrensi.js
+++ js/refrensi.js
@@ -4,7 +4,7 @@ var customers = [
 	[2, "Bryant, Ed. 2014", "Bryant, Edward. 2014. Tsunami : The Underrated Hazard. Springer"],
 	[3, "Faradiba, Nadia. 2021", "Potensi Tsunami Jawa Timur 29 Meter, Ini Cara Mitigasi Bencana Tsunami. Diakses pada tanggal 21 Juni 2021 pukul 00.30 WIB. https://www.kompas.com/sains/read/2021/06/01/173500623/potensi-tsunami-jawa-timur-29-meter-ini-cara-mitigasi-bencana-tsunami?page=all"],
 	[4, "Hamzah, L. dkk., 2000", "Hamzah, L., N. T. Puspito, and F. Imamura, Tsunami catalog and zones in Indonesia, Journal of Natural Disaster Science, 22, 25–43, 2000."],
-	[5, "Ibtihaj, Ihda. 2021", "Ibtihaj, Ihda. 2021. Indonesia Paleotsunami Database, Universitas Indonesia. 21 Juni 2021"],
+	[5, "Ibtihaj, Ihda. 2021", "Indonesia Paleotsunami Database, Universitas Indonesia. Diakses dari https://trc-ui.geosinyal.id pada 21 Juni 2021 pukul 15:41 WIB"],
 	[6, "Kementerian Energi dan Sumber Daya Mineral (n.d)", "Kementerian Energi dan Sumber Daya Mineral (n.d). Pengenalan Tsunami. Diakses pada 22 Juni 2021 pukul 21.11 WIB. https://www.esdm.go.id/assets/media/content/Pengenalan_Tsunami.pdf"],
 	[7, "Sugito, N. 2008", "Sugito, N. 2008. Tsunami. Universitas Pendidikan Indonesia : Program Studi Pendidikan Geografi. Diakses pada tanggal 22 Juni 2021 pukul 21.10 WIB. http://file.upi.edu/Direktori/FPIPS/JUR._PEND._GEOGRAFI/198304032008012-NANIN_TRIANA_SUGITO/TSUNAMI.pdf"],
 	[8, "Tsuji, Y., F. Imamura dkk., 1995", "Tsuji, Y., F. Imamura, H. Matsutomi, C.E. Synolakis, P.T. Nanang, Jumadi, S. Harada, S.S. Han, K. Arai and B Cook, 1995b, Field survey of the East Java earthquake and tsunami June 3, 1994, In Topical Issues on Tsunami 1992-94, Pure and Applied Geophysics, 144, 3/4, 839-854."]
